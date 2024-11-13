0e55bc4f8c936b5bb19ec9c26cf371084086155b
William Mårback <william.marback@hotmail.com>
2024-11-13 16:59:09 +0100

    Maybe better? Idk

--- 1b.py
+++ 1b.py
@@ -2,15 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define parameters
-Tb = 0.0001  # Width of the baseband pulse in seconds
 f_c = 3500  # Carrier frequency in Hz
 w_c = 2 * np.pi * f_c  # Carrier angular frequency in rad/s
+n = 1
+M = 2
+Tb = 5 / f_c  # Width of the baseband pulse in seconds
 
 # Frequency range (omega values)
 omega = np.arange(-5 * w_c, 5 * w_c, 0.5)
 
 # Define X_b(omega) as a sinc function (assuming a pulse of width Tb)
-X_b = lambda omega_2: Tb * np.sinc(omega_2 * Tb / (2 * np.pi))
+def X_b(omega_2):
+    return Tb * np.sinc(omega_2 * Tb / (2 * np.pi)) * \
+                        np.exp(-1j * omega_2 * ((2 * n + 1) * Tb) / 2)
 
 # Calculate the expression j/2 * (X_b(omega) * (omega + w_c) - X_b(omega) * (omega - w_c))
 j_over_2 = 1j / 2
@@ -31,7 +35,7 @@ plt.plot(omega, expression.imag, label="Imaginary Part", color="red")
 plt.title("Imaginary Part of the Expression")
 plt.xlabel("Frequency (rad/s)")
 plt.ylabel("Amplitude")
-#plt.xlim(-5000, 5000)
+plt.xlim(-5000, 5000)
 plt.grid(True)
 
 plt.tight_layout()
