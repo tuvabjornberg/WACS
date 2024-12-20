21cb75179959889fad1772e6404e343e8ecd5209
Matilda <matilda.stenbaek.1554@student.uu.se>
2024-12-20 16:00:41 +0100

    receiver done

--- receiver.py
+++ receiver.py
@@ -22,6 +22,9 @@ def filter_lp(f_pass, f_stop, A_pass, A_stop, f_sample):
 
 
 def demodulator(f_carrier, y, f_stop, A_pass, A_stop, f_sample):
+    print(y)
+    y = y[:, 0]
+    print(y)
 
     t = np.arange(len(y)) / f_sample
 
@@ -44,15 +47,18 @@ def main():
     f_pass = (3475, 3525)
     f_stop = (3450, 3550)
 
-    A_pass = 3  # passband ripples
+    A_pass = 0.1  # passband ripples
     A_stop = 40  # stopband attenuation
 
     f_carrier = 3500
     A_carrier = 1  # amplitude of input signal
 
+    expected = "Hello World!"
+    expected_bits = wcs.encode_string(expected)
+
     ellip_filter_b, ellip_filter_a = filter_bp(f_pass, f_stop, A_pass, A_stop, fs)
     
-    y = sd.rec(20*fs, fs, channels=1, blocking=True)
+    y = sd.rec(int(20* fs), fs, channels=1, blocking=True)
     sd.wait()
     print("Recording done")
 
@@ -65,9 +71,18 @@ def main():
     print("demodulation done")
 
     br = wcs.decode_baseband_signal(ybm, ybp, Tb, fs)
+    # print(br)
+
+    print("Expected bits:" + str(len(expected_bits)))
+    counter = 0
+    for i in range(len(expected_bits)-1):
+        if not (expected_bits[i] == 1 and br[i] == True or expected_bits[i] == 0 and br[i] == False):
+            counter+=1
+    
+    print("Incorrect bits: " + str(counter))
     data_rx = wcs.decode_string(br)
     print("Received: " + data_rx)
 
-
-    
+if __name__ == "__main__":
+    main()
     
